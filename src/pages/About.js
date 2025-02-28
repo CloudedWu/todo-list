26d87c32e7f982d8b25e88faca3b02dea5b700b1
Wu <749830819@qq.com>
2025-02-28 15:44:15 +0800

    test

--- src/pages/About.js
+++ src/pages/About.js
@@ -5,9 +5,7 @@ export default function About() {
     <div>
       <h1>Hello!world</h1>
       <h1>test message</h1>
-      <h2>hello!</h2>
       <button>nothing</button>
-      <p>测试测试</p>
       <p>test分支</p>
     </div>
   )
